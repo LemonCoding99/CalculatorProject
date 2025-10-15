db3f333fd76e4906910368c501339c1f98ece1c2
LemonCoding99 <qordmstj7530@gmail.com>
2025-10-15 17:44:10 +0900

    feat: implement input functionality

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -1,7 +1,19 @@
 package level1;
 
+import java.util.Scanner;
+
 public class Main {
-    static void main() {
-        
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 입력받기
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int number1 = scanner.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int number2 = scanner.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        String operator = scanner.next();
+
     }
 }
+
